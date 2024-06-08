c9787f6bb974f299992d76c7f5d0dea8f20b9d42
bfarach <basilio.farach@southworks.com>
2024-06-07 18:22:51 -0600

    remove not needed import

--- pinecone-indexer/text_chunks.py
+++ pinecone-indexer/text_chunks.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from langchain_community.document_loaders import TextLoader
-from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
 
